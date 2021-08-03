517176dc966a455b31a1a58777cc7e3c0319d030
PolonevichA <andrey.polonevich0209@gmail.com>
2021-08-03 22:22:08 +0300

    modals

---
+++ src/js/modules/modals.js
@@ -0,0 +1,77 @@
+const modals = () => {
+
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+
+        const triggers = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector),
+              windows = document.querySelectorAll('[data-modal]'),
+              scroll = calcScroll();
+
+        triggers.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if(e.target){
+                    e.preventDefault();
+                }
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+    
+                modal.style.display = 'block';
+                document.body.style.overflow ='hidden';
+                document.body.style.marginRight = `${scroll}px`;
+            })
+        })
+
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            modal.style.display = 'none';
+            document.body.style.overflow ='';
+            document.body.style.marginRight = `0px`;
+        })
+
+        modal.addEventListener('click', (e) => {
+            if(e.target === modal && closeClickOverlay){
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = 'none';
+                document.body.style.overflow ='';
+                document.body.style.marginRight = `0px`;
+            }
+        })
+    }
+
+    function calcScroll(){
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+    function showModalByTime(selector, time){
+        setTimeout(() => {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow ='hidden';
+        }, time)
+    }
+
+    bindModal();
+
+    // showModalByTime('.popup', 60000)
+};
+
+export default modals;
